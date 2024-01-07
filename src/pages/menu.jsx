3249051aae2b9544525387104f07261f3680bae8
Rachid Boudjelida <rachidboudjelida@gmail.com>
2024-01-07 22:19:52 +0100

    fix filters

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -10,11 +10,8 @@ import { useState } from "react";
 
 const Menu = () => {
   const [eat, setEat] = useState(data);
-    const [search, setSearch] = useState("All");
-    const [repas, setRepas] = useState("All");
-
-  
-
+  const [search, setSearch] = useState("All");
+  const [repas, setRepas] = useState("All");
 
   return (
     <div className=" flex flex-col mt-[-00px]  h-screen w-full">
@@ -23,72 +20,38 @@ const Menu = () => {
         Menu
       </button>
       <div className=" mt-5 flex  h-hull justify-center  items-start">
-          <select name="" id="" className=" text-lg pt-3 flex justify-start items-start text-start btn btn-outline   focus: btn-error     w-1/2 h-10 rounded-xl text-black hover:border-b-4">
-            <option value={repas} onClick={() => {
-              setRepas("All");
-            }}>All</option>
-            <option value={repas} onClick={() => {
-              setRepas("pizza");
-            }}>pizza</option>
-            <option value={repas} onClick={() => {
-              setRepas("burger");
-            }}>burger</option>
-            <option value={repas} onClick={() => {
-              setRepas("turque");
-            }}>turque</option>
-            <option value={repas} onClick={() => {
-              setRepas("pates");
-            }}>pates</option>
-            <option value={repas} onClick={() => {
-              setRepas("indien");
-            }}>indien</option>
-            <option value={repas}  onClick={() => {
-              setRepas("asiatique");
-            }}>asiatique</option>
-            <option value={repas} onClick={() => {
-              setRepas("tacos");
-            }}>Tacos</option>
-            <option value={repas} onClick={() => {
-              setRepas("kabab");
-            }}>kabab</option>
-            <option value={repas} onClick={() => {
-              setRepas("syrien");
-            }}>syrien</option>
-            <option value={repas} onClick={() => {
-              setRepas("street food");
-            }}>street food</option>
-            <option value={repas} onClick={() => {
-              setRepas("grillades");
-            }}>Grillades</option>
-            <option value={repas} onClick={() => {
-              setRepas("italien");
-            }}>italien</option>
-            <option value={repas}  onClick={() => {
-              setRepas("traditionel");
-            }}>traditionel</option>
-            <option value={repas} onClick={() => {
-              setRepas("sandwiches");
-            }}>sandwiches</option>
-            <option value={repas} onClick={() => {
-              setRepas("crepes");
-            }}>Crêpes</option>
-            <option value={repas} onClick={() => {
-              setRepas("libanais");
-            }}>Libanais</option>
-            <option value={repas}  onClick={() => {
-              setRepas("boissons");
-            }}>boissons</option>
-            <option value={repas}  onClick={() => {
-              setRepas("desserts");
-            }}>desserts</option>
-            <option value={repas} onClick={() => {
-              setRepas("patisserie");
-            }}>patisserie</option>
-          </select>
-
+        <select
+          value={repas}
+          onChange={(e) => {
+            setRepas(e.target.value);
+          }}
+          name=""
+          id=""
+          className=" text-lg pt-3 flex justify-start items-start text-start btn btn-outline   focus: btn-error     w-1/2 h-10 rounded-xl text-black hover:border-b-4"
+        >
+          <option value={"All"}>All</option>
+          <option value={"pizza"}>pizza</option>
+          <option value={"burger"}>burger</option>
+          <option value={"turque"}>turque</option>
+          <option value={"pates"}>pates</option>
+          <option value={"indien"}>indien</option>
+          <option value={"asiatique"}>asiatique</option>
+          <option value={"tacos"}>Tacos</option>
+          <option value={"kabab"}>kabab</option>
+          <option value={"syrien"}>syrien</option>
+          <option value={"street food"}>street food</option>
+          <option value={"grillades"}>Grillades</option>
+          <option value={"italien"}>italien</option>
+          <option value={"traditionel"}>traditionel</option>
+          <option value={"sandwiches"}>sandwiches</option>
+          <option value={"crepes"}>Crêpes</option>
+          <option value={"libanais"}>Libanais</option>
+          <option value={"boissons"}>boissons</option>
+          <option value={"desserts"}>desserts</option>
+          <option value={"patisserie"}>patisserie</option>
+        </select>
       </div>
 
-      
       <div className=" flex justify-center mt-10 flex-wrap  ">
         <p className=" text-xl m-20">
           Thousands of items delivered to your door:{" "}
@@ -99,43 +62,47 @@ const Menu = () => {
           minutes. Discover our rapidly growing offer and order conveniently at
           supermarket prices!
         </p>
-        {data.map((key,i) => {
-                      // if (
-                      //   (key.type.toLowerCase().includes(search.toLowerCase()) &&repas == "All") ||repas == key.type
-                      // )
-                        return (
-                          <div className=" bg-gray-200 gap-10  cart w-1/4 flex-wrap text-left m-10 p-4 h-screen border border-black rounded-lg  ">
-                            <img
-                              src={key.lien}
-                              alt=""
-                              className="h-72 w-full"
-                            />
-                            <h1 className=" max-h-min-[10px]:  text-xl font-bold m-2">
-                              {key.title}
-                            </h1>
-                            <p className=" uppercase">*{key.type}*</p>
-                            <p className=" my-4 ">
-                              <span className=" text-gray-500">le temp est</span>
-                              <span className="text-xl  mx-5 underline  text-red-900">
-                                {key.time}
-                              </span>
-                            </p>
-                            <label htmlFor="" className=" text-xl font-bold ">
-                              prix =
-                            </label>
-                            <button className=" my-4 text-lg text-black ml-3 bg-green-600 w-1/2 h-10 rounded-3xl hover:text-xl ">
-                              {key.price}
-                            </button>
-                            <div className=" flex gap-1 my-10 ">
-                              <button className="btn btn-outline bg-red-600  w-1/2 ">
-                                <Link to="/info"> learn mor</Link>
-                              </button>
-                              <button className="btn  btn-outline btn-error w-1/2 ">
-                                Ajoute
-                              </button>
-                            </div>
-                          </div>
-                        );
+        {data.map((key, i) => {
+          // if (
+          //   (key.type.toLowerCase().includes(search.toLowerCase()) &&repas == "All") ||repas == key.type
+          // )
+          if (
+            repas == "All" ||
+            key.type.toLowerCase().includes(search.toLowerCase()) ||
+            repas == key.type
+          )
+            return (
+              <div
+                key={i}
+                className=" bg-gray-200 gap-10  cart w-1/4 flex-wrap text-left m-10 p-4 h-screen border border-black rounded-lg  "
+              >
+                <img src={key.lien} alt="" className="h-72 w-full" />
+                <h1 className=" max-h-min-[10px]:  text-xl font-bold m-2">
+                  {key.title}
+                </h1>
+                <p className=" uppercase">*{key.type}*</p>
+                <p className=" my-4 ">
+                  <span className=" text-gray-500">le temp est</span>
+                  <span className="text-xl  mx-5 underline  text-red-900">
+                    {key.time}
+                  </span>
+                </p>
+                <label htmlFor="" className=" text-xl font-bold ">
+                  prix =
+                </label>
+                <button className=" my-4 text-lg text-black ml-3 bg-green-600 w-1/2 h-10 rounded-3xl hover:text-xl ">
+                  {key.price}
+                </button>
+                <div className=" flex gap-1 my-10 ">
+                  <button className="btn btn-outline bg-red-600  w-1/2 ">
+                    <Link to="/info"> learn mor</Link>
+                  </button>
+                  <button className="btn  btn-outline btn-error w-1/2 ">
+                    Ajoute
+                  </button>
+                </div>
+              </div>
+            );
         })}
       </div>
     </div>
